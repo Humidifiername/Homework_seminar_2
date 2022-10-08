becf750ff7d838bb8678e727b3f16c0090d369a9
Albert <albert.fedorov.99@bk.ru>
2022-10-08 05:22:54 +0300

    13 задание

---
+++ Task_13/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num < 100)
+{
+    Console.Write(num + " - третьей цифры нет");
+}
+else if (num > 99)
+{
+    Console.WriteLine((num % 100) % 10);
+}
